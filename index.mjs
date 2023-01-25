d1663c33c16f4da43a946c00adb40fbe2969572e
fritylo <to.fritylo@gmail.com>
2023-01-25 23:19:34 +0300

    feat: added help

--- index.mjs
+++ index.mjs
@@ -30,11 +30,42 @@ function execWithLog(command) {
     return res;
 }
 
+function help() {
+    console.log(`
+================================================
+Git command to revert merge commits without pain
+================================================
+
+Usage:
+    revoke <merge_commit_hash>
+    revoke 928e58ad
+        
+Before usage:
+    Checkout branch with merge commit
+
+How it works:
+    1) Delete local branch of merge (if present)
+    2) Delete remote branch of merge (if present)
+    3) Revert merge commit
+    4) Create copy of merge branch
+    5) Cherry-pick merge changes to the copy
+    6) Print suggestions for changes pushing
+
+© fritylo 2023, license MIT
+    `);
+}
+
 function main() {
     const argv = process.argv.slice(2);
 
     if (argv.length === 0) {
         console.log('No hash provided. Exit...');
+        help();
+        return;
+    }
+
+    if (argv[0] === '--help') {
+        help();
         return;
     }
 
